c0f655d468698b5ea61e2624721518ab6fd3e636
Gilbert Kim <gbertkim@gmail.com>
2019-07-07 04:28:33 -0700

    added services

--- src/store.js
+++ src/store.js
@@ -1,21 +1,20 @@
-
 const uuid = require('uuid/v4')
 
 const bookmarks = [
   { id: uuid(),
-    title: 'Google',
-    url: 'https://www.google.com',
-    description: 'Search website',
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
     rating: 5 },
   { id: uuid(),
-    title: 'twitter',
-    url: 'https://www.twitter.com',
-    description: 'Social news feed',
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
     rating: 4 },
   { id: uuid(),
-    title: 'Youtube',
-    url: 'https://www.youtube.com',
-    description: 'Videos',
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
     rating: 5 },
 ]
 
